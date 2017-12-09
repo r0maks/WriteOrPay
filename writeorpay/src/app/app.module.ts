cdc70d72054f46143455185055d89b3d32586b72
r0maks <rrmaksimov@gmail.com>
2017-12-08 22:28:56 -0500

    Generating first app component - note editor component

--- writeorpay/src/app/app.module.ts
+++ writeorpay/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NoteEditorComponent } from './note-editor/note-editor.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NoteEditorComponent
   ],
   imports: [
     BrowserModule
