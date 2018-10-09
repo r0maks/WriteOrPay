dc35adbf54b764f7d6f5b223716062fef26fcc27
Roman Maksimov <rrmaksimov@gmail.com>
2018-10-08 22:14:53 -0400

    started work on side panel

--- writeorpay/src/app/app.module.ts
+++ writeorpay/src/app/app.module.ts
@@ -4,13 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NoteEditorComponent } from './note-editor/note-editor.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { CountTileComponent } from './count-tile/count-tile.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NoteEditorComponent,
-    SidebarComponent
+    SidebarComponent,
+    CountTileComponent,
   ],
   imports: [
     BrowserModule
---
+++ writeorpay/src/app/count-tile/count-tile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-count-tile',
+  templateUrl: './count-tile.component.html',
+  styleUrls: ['./count-tile.component.scss']
+})
+export class CountTileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
