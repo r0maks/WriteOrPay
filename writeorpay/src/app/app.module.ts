f730884cff6956c6ee63fae38bdb675c472930ca
Roman Maksimov <rrmaksimov@gmail.com>
2018-10-09 22:41:15 -0400

    WIP

--- writeorpay/src/app/app.module.ts
+++ writeorpay/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { NoteEditorComponent } from './note-editor/note-editor.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
