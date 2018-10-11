54e8ab57c12f84ab9f03333bf8a34b706407d063
Roman Maksimov <rrmaksimov@gmail.com>
2018-10-10 20:24:40 -0400

    fixed start of the month issue

--- writeorpay/src/app/calendar-pane/calendar-pane.component.ts
+++ writeorpay/src/app/calendar-pane/calendar-pane.component.ts
@@ -27,22 +27,25 @@ export class CalendarPaneComponent implements OnInit {
     if (!this.selectedMonth) {
       this.selectedMonth = moment();
     }
+    const start = this.selectedMonth.startOf('month').day();
 
+    // offset is number of empty days to fill before starting to count
+    const offset = start - 1;
     this.yearText = this.selectedMonth.format('YYYY');
     this.monthText = this.selectedMonth.format('MMMM');
     this.numDaysInMonth = this.selectedMonth.daysInMonth();
     
     this.daysInMonth = [];
+    this.offsetCalendarDays(offset);
+
     for (let i = 0; i < this.numDaysInMonth; i++) {
       this.daysInMonth.push({date: i + 1});
     }
   }
 
-  getColor(day) {
-    if (day.date > 6) {
-      return 'green';
-    } else {
-      return 'red';
+  private offsetCalendarDays(offset: number): void{
+    for (let i = 0; i < offset; i++) {
+      this.daysInMonth.push({}); // blank objects
     }
   }
 
