010055b71be9a7e79ad85adf2f08274c806738c2
Román Maksimov <rrmaksimov@gmail.com>
2018-10-23 10:15:20 -0400

    fixed padding issue and added a note with a super long title for testing

--- writeorpay/src/app/store/testdata.ts
+++ writeorpay/src/app/store/testdata.ts
@@ -33,6 +33,10 @@ export function getTestData(): Note[] {
     'Lorem ipsum dolor amet mustache knausgaard +1, blue bottle waistcoat tbh semiotics artisan synth stumptown gastropub cornhole celiac swag. Brunch raclette vexillologist post-ironic glossier ennui XOXO mlkshk godard pour-over blog tumblr humblebrag. Blue bottle put a bird on it twee prism biodiesel brooklyn. Blue bottle ennui tbh succulents.'
     ));
 
+    notes.push(new Note('This is a test of a long title. How does it look when it is super long like this? Mote text can go here and it should not wrap',
+    'Lorem ipsum dolor amet mustache knausgaard +1, blue bottle waistcoat tbh semiotics artisan synth stumptown gastropub cornhole celiac swag. Brunch raclette vexillologist post-ironic glossier ennui XOXO mlkshk godard pour-over blog tumblr humblebrag. Blue bottle put a bird on it twee prism biodiesel brooklyn. Blue bottle ennui tbh succulents.'
+    ));
+
     let date = moment();
 
     notes.forEach(n => {
