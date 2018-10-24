4d84e362cac26cd6cf79c6f0c7ff7c5d1a594664
Román Maksimov <rrmaksimov@gmail.com>
2018-10-23 23:20:50 -0400

    WIP

---
+++ writeorpay/src/app/services/api.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+
+@Injectable()
+export class ApiService {
+
+
+  constructor() {
+
+  }
+
+
+}
+
